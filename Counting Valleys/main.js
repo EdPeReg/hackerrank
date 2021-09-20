399dfaa082f6a2d8005113931295185149a8c216
EdPeReg <dardo1818@gmail.com>
2021-09-19 22:26:19 -0500

    Nodejs - Simplified using reduce()

--- Counting Valleys/main.js
+++ Counting Valleys/main.js
@@ -1,17 +1,17 @@
+// https://dev.to/sagar/three-dots---in-javascript-26ci#:~:text=When%20three%20dots%20(%E2%80%A6)%20is,an%20array%20into%20a%20list.
 
 function countingValleys(steps, path) {
     let level = 0;
-    let valleys = 0;
 
-    for(let i = 0; i < steps; ++i) {
-        if(path[i] == 'U') ++level;
-        if(path[i] == 'D') --level;
-
-        if(path[i] == 'U' && level == 0)
-            ++valleys;
-    }
-
-    return valleys;
+    // ... operator will spread the path, getting an array of characters.
+    // This allows us to iterate in each value using reduce(), the most important
+    // thing is the accumulator (sum) that will be added in each iteration and our 
+    // value, which it's the current value in each iteration.
+    return [...path].reduce((sum, value) => {
+        if(value == 'U') { ++level; } else { --level }
+        if(value == 'U' && level == 0) { ++sum; }
+        return sum;
+    }, 0);
 }
 
 function main() {
