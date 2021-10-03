cb98f14d7002eb92a602b922a26f32f9974e8f10
EdPeReg <dardo1818@gmail.com>
2021-10-02 21:54:07 -0500

    Jumping on clouds C++ - version 2

--- Jumping on the clouds/main.cpp
+++ Jumping on the clouds/main.cpp
@@ -4,11 +4,11 @@
 int jumpingOnClouds(std::vector<int> c)
 {
     int jumps = 0;
-    const int CLOUD = 0;
 
     for(size_t i = 0; i < c.size() - 1;)
     {
-        (c[i+2] == CLOUD) ? i += 2 : ++i;
+        // Remember that we have an array of 1 = true or 0 = false.
+        c[i+2] ? ++i : i += 2;
         ++jumps;
     }
 
